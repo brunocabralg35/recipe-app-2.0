838d566e2d86db025cee7b33cf2e961516b32325
Bruno Cabral <brunocabral035@gmail.com>
2022-04-07 19:35:03 -0300

    first commit with popular page fetching data

---
+++ src/components/Veggie.jsx
@@ -0,0 +1,5 @@
+export default function Veggie() {
+  return <div>Veggie
+    <hr />
+  </div>;
+}
---
+++ src/pages/Pages.jsx
@@ -0,0 +1,13 @@
+import Home from "./Home";
+
+function Pages() {
+  return (
+    <div>
+        Pages
+        <hr></hr>
+        <Home/>
+    </div>
+  )
+}
+
+export default Pages
